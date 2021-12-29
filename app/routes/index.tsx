e27018c71738b0df5898a9c12ba692664aa0256c
Christian Hamburger Grøngaard <christianhg@gmail.com>
2021-12-29 10:46:07 +0100

    Replicate bug

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,14 @@
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
+      <h1>Vercel/Remix bug</h1>
+      <p>The image below is fetched by making a request to the <code>/unsplash</code> resource route which in turn makes a request to <code>unsplash.com</code>.</p>
+      <p>This works locally, but not when deployed to Vercel.</p>
+      <img src="/unsplash" />
+      <p>
+        Photo by{' '}
+        <a href="https://unsplash.com/photos/fbhqOlTZjro">Ümit Yıldırım</a>
+      </p>
     </div>
   );
 }
---
+++ app/routes/unsplash.ts
@@ -0,0 +1,7 @@
+import { LoaderFunction } from 'remix';
+
+export const loader: LoaderFunction = () => {
+  return fetch(
+    'https://images.unsplash.com/photo-1640703932573-901de6acf4b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2066&q=80"'
+  );
+};
